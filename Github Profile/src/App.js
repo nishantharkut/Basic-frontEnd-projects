fa2489e70a8f5c929463705acf4af37bfe38691d
Nishant Harkut <nhnishantharkut@gmail.com>
2024-09-13 20:48:28 +0530

    Update 13 September

---
+++ Github Profile/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+import Particle from "./Particles.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <Particle />
+    </div>
+  );
+}
+
+export default App;
---
+++ Github Profile/src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import logo from './profile.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1 className="title">Nishant Harkut</h1>
+        <h5>Welcome to my Github Profile!</h5>
+      </div>
+    );
+  }
+}
+
+export default Header;
